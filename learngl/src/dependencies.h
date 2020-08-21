38d2bb73d4e08c0c0e220dec835d887b823716b3
sri harsha nandigam <nandigamharsha@gmail.com>
2020-08-21 10:59:35 +0530

    Updated to 64bit support

--- learngl/src/dependencies.h
+++ learngl/src/dependencies.h
@@ -1,14 +1,13 @@
 #pragma once
-#ifdef WIN32
+
 #include <Windows.h>
-#include"glew.h"
+#include"GL/glew.h"
 #include<math.h>
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<vector>
 #include"GLFW/glfw3.h"
-#endif // WIN32
 
 
 
